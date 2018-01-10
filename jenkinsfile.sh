a12c558096cc004ed7676353a429f2c4dcc9da56
vidwan05 <33649368+vidwan05@users.noreply.github.com>
2018-01-09 19:18:58 -0500

    new commit

--- jenkinsfile.sh
+++ jenkinsfile.sh
@@ -12,7 +12,7 @@ pipeline {
                 }
             }
         }
-        stage ('Deploy to Staging'){
+        stage ('Deploy to Stagin{
             steps {
                 build job: 'Deploy-to-staging'
             }
